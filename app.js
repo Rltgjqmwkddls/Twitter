592f6215df9163a5e3de52e1dc202cf6bbd7d28e
Rltgjqmwkddls <spdlqjdkdlel0798@naver.com>
2024-04-26 15:01:46 +0900

    twitter 서버 초기 세팅

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express"
+import morgan from "morgan"
+import tweetsRouter from '/router/tweets.js'
+
+const app = express()
+
+app.use(express.json())
+app.use(morgan("dev"))
+
+app.use('/twwets', tweetsRouter)
+
+app.use((req, res, next) => {
+    res.status(404)
+})
+
+app.listen(8080)
+
