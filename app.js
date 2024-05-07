65a155c1607569c7b03cbb9501dced951441c9b0
Rltgjqmwkddls <spdlqjdkdlel0798@naver.com>
2024-05-08 00:52:59 +0900

    aaaaa

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(morgan("dev"))
 
-app.use('/twwets', tweetsRouter)
+app.use('/tweets', tweetsRouter)
 app.use('/auth', authRouter)
 
 app.use((req, res, next) => {
