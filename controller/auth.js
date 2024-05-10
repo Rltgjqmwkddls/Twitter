457f0b17f4e18ce4b2143c5b76e062602f8009b6
Rltgjqmwkddls <spdlqjdkdlel0798@naver.com>
2024-05-10 16:51:26 +0900

    Mongoose 적용 + 수정

--- controller/auth.js
+++ controller/auth.js
@@ -5,46 +5,46 @@ import { config } from '../config.js';
 
 function createJwtToken(id){
     return jwt.sign({id}, config.jwt.secretKey,
-        {expiresIn: config.jwt.expiresInSec})
+        {expiresIn: config.jwt.expiresInSec});
 }
 
 export async function signup(req, res, next){
-    let {username, password, name, email, url} = req.body
-    const found = await authRepository.findByUsername(username)
+    let {username, password, name, email, url} = req.body;
+    const found = await authRepository.findByUsername(username);
     if(found){
-        return res.status(409).json({message:`${username}이 이미 있습니다`})
+        return res.status(409).json({message:`${username}이 이미 있습니다`});
     }
-    password = await bcrypt.hash(password, config.bcrypt.saltRounds)
-    const userId = await authRepository.createUser({username, password, name, email, url})
-    const token = createJwtToken(userId)
-    res.status(201).json({token, username})
+    password = await bcrypt.hash(password, config.bcrypt.saltRounds);
+    const userId = await authRepository.createUser({username, password, name, email, url});
+    const token = createJwtToken(userId);
+    res.status(201).json({token, username});
 }
 
 export async function login(req, res, next){
     const {username, password} = req.body;
     // const user = await authRepository.login(username);
-    const user = await authRepository.findByUsername(username)
+    const user = await authRepository.findByUsername(username);
     if(!user){
-        return res.status(401).json({message: `아이디를 찾을 수 없음`})
+        return res.status(401).json({message: `아이디를 찾을 수 없음`});
     }
-    const isValidpassword = await bcrypt.compareSync(password, user.password)
+    const isValidpassword = await bcrypt.compareSync(password, user.password);
     if(!isValidpassword){
-        return res.status(401).json({message: `비밀번호가 틀렸음`})
+        return res.status(401).json({message: `비밀번호가 틀렸음`});
     }
-    const token = createJwtToken(user.id)
-    res.status(200).json({token, username})
+    const token = createJwtToken(user.id);
+    res.status(200).json({token, username});
 }
 // export async function verify(req, res, next){
-//     const token = req.header['Token']
+//     const token = req.header['Token'];
 //     if(token){
 //         res.status(200).json(token);
 //     }
 // }
 
 export async function me(req, res, next){
-    const user = await authRepository.findById(req.userId)
+    const user = await authRepository.findById(req.userId);
     if(!user[0][0]){
-        return res.status(404).json({message: `일치하는 사용자가 없음`})
+        return res.status(404).json({message: `일치하는 사용자가 없음`});
     }
-    res.status(200).json({token: req.token, username: user.username})
+    res.status(200).json({token: req.token, username: user.username});
 }
